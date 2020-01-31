050a86cfe51ae0405cc4a44e34c28f7f71ec3ccc
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-01-31 00:16:30 +0000

    Yearly Investment

--- CInterest.py
+++ CInterest.py
@@ -8,3 +8,7 @@ principal = float(raw_input('Enter current amount in account: '))
 
 print('How much money do you plan on investing monthly?')
 monthly_invest = float(raw_input('Enter amount: '))
+
+print('What do you estimate will be the yearly interest of this investment?')
+interest = float(raw_input('Enter interest in decimal numbers (10% = 0.1):'))
+
