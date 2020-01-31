a25481b0d48ca7168bbd599adae367e609d23f16
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-01-31 00:15:18 +0000

    Money Currently in the account

--- CInterest.py
+++ CInterest.py
@@ -3,3 +3,5 @@
 print('How many years will you be saving?')
 years = int(raw_input('Enter Years:'))
 
+print('How much money is currently in your account?')
+principal = float(raw_input('Enter current amount in account: '))
