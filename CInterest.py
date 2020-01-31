f07d55305090a756cb07ea33eef56d97a2a93ff0
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-01-31 00:15:51 +0000

    Money You Plan on Investing

--- CInterest.py
+++ CInterest.py
@@ -5,3 +5,6 @@ years = int(raw_input('Enter Years:'))
 
 print('How much money is currently in your account?')
 principal = float(raw_input('Enter current amount in account: '))
+
+print('How much money do you plan on investing monthly?')
+monthly_invest = float(raw_input('Enter amount: '))
