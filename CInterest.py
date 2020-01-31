815703ccf7c3b2e1329edbaccb58d472327b7dec
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-01-31 00:17:49 +0000

    Program Complete

--- CInterest.py
+++ CInterest.py
@@ -21,4 +21,6 @@ for i in range(0, years):
     if final_amount == 0:
         final_amount = principal 
 
-   
+    final_amount = (final_amount + monthly_invest) * (1 + interest)
+
+print('This is how much money you would have in your account after {} years: '.format(years) + str(final_amount))
